2c0dba3e6f9417f82c1920478070fea17773bc09
8b7 <himoo470@gmail.com>
2020-01-31 23:12:57 +0300

    tdtd

---
+++ Assets/Script/Sound.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+
+    private AudioSource _audioSource;
+    public GameObject SoundPlay;
+    public GameObject SoundMute;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic()
+    {
+        SoundMute.SetActive(true);
+        _audioSource.Stop();
+        if(SoundPlay == true)
+        {
+            SoundPlay.SetActive(false);
+        }
+    }
+    public void MuteMusic()
+    {
+        _audioSource.Play();
+        SoundPlay.SetActive(true);
+        if (SoundMute == true)
+        {
+            SoundMute.SetActive(false);
+        }
+    }
+
+}
